86a2b107838e403116027c5fd3159463ee0e1295
piyush121 <piyushchaudhary07@gmail.com>
2016-03-01 15:15:50 -0500

    first commit

--- src/tweetMap/Main.java
+++ src/tweetMap/Main.java
@@ -1,41 +1,52 @@
 package tweetMap;
+
 import java.util.List;
 
 import twitter4j.*;
 import twitter4j.auth.AccessToken;
 
+/**
+ * @author Piyush Chaudhary
+ *
+ */
 public class Main {
 	public static void main(String[] args) {
-		
+
 		final Twitter twitter = new TwitterFactory().getInstance();
-	    final  AccessToken accessToken = new AccessToken("39964732-rBS2TnH2wQ52VuQZjodzyJwku49byY8WdWf3h9USA", "cfTLD9cEpKaypEJ6Xzt1BPZyRondv8lqtQtjlBFmCfRlb");
-	    twitter.setOAuthConsumer("wexQs0nZ1Mfq5rZAAiXFgVHnz", "2df8sukTaanmptybHxEa1Xi1KxzVxlgkPmjUpwcu6LPdgKH3nX");
-	    twitter.setOAuthAccessToken(accessToken);
+		final AccessToken accessToken = new AccessToken("39964732-rBS2TnH2wQ52VuQZjodzyJwku49byY8WdWf3h9USA",
+				"cfTLD9cEpKaypEJ6Xzt1BPZyRondv8lqtQtjlBFmCfRlb");
+		twitter.setOAuthConsumer("wexQs0nZ1Mfq5rZAAiXFgVHnz", "2df8sukTaanmptybHxEa1Xi1KxzVxlgkPmjUpwcu6LPdgKH3nX");
+		twitter.setOAuthAccessToken(accessToken);
 
-	    try {
-            Query query = new Query();
-            query.geoCode(new GeoLocation(37.781157,-122.398720),100.0,"mi");
-            QueryResult result;
-            System.out.println("Searching...");
-            int Count=0;
+		try {
+			Query query = new Query("#iphone6");
 
-            do {
-                result = twitter.search(query);
-                List<Status> tweets = result.getTweets();
-                for (Status tweet : tweets) {
-                	if(tweet.getGeoLocation()!=null)
-                		System.out.println(tweet.getGeoLocation());
-                }
-            }
-            while ((query = result.nextQuery()) != null);
-            System.out.println(Count);
-            System.exit(0);
-        } catch (TwitterException te) {
-            te.printStackTrace();
-            System.out.println("Failed to search tweets: " + te.getMessage());
-            System.exit(-1);
-        }
+			// query.geoCode(new
+			// GeoLocation(37.781157,-122.398720),3900.0,"mi");
+
+			QueryResult result;
+			System.out.println("Searching...");
+			int count = 0;
+
+			do {
+				result = twitter.search(query);
+
+				List<Status> tweets = result.getTweets();
+				for (Status tweet : tweets) {
+					{ // if(tweet.getGeoLocation()!=null)
+						System.out.println(tweet.getText());
+						count++;
+					}
+				}
+
+			} while ((query = result.nextQuery()) != null);
+			System.out.println(count);
+		} catch (TwitterException te) {
+			te.printStackTrace();
+			System.out.println("Failed to search tweets: " + te.getMessage());
+			System.exit(-1);
+		}
+
 	}
-	
 
 }
